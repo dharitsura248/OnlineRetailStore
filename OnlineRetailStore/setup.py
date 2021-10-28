3c2fedd3fda1a082ecaed1832a19fa2bbf521165
dharitsura248 <dharit_sura24@yahoo.com>
2021-10-28 00:07:04 -0700

    Initial Check in

---
+++ OnlineRetailStore/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The idea of the project is to perform market basket analysus and derive recommendations for the most frequently bought items together. Based on the time of the year, we can also determine the demand and supply of the items that need to be stocked. ',
+    author='Dharit Sura',
+    license='',
+)
